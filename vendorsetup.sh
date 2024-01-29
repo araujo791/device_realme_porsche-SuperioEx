14a3cad83e3a64e629f7762e717bd598773266e3
“araujo791” <anderson.henrique@gmx.com>
2024-01-29 11:05:28 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,19 +2,19 @@ echo "Cloning Repos..."
 echo "Cloning Common Device Tree..."
 git clone https://github.com/araujo791/device_oneplus_sm8350-common-SuperioEx -b udc device/oneplus/sm8350-common
 echo "Cloning Hardware Tree..."
-git clone https://github.com/IamCexary/hardware_oplus.git -b udc hardware/oplus
+git clone https://github.com/araujo791/hardware_oplus-SuperiorEx -b udc hardware/oplus
 echo "Cloning rashoumon Kernel Source..."
-git clone https://github.com/IamCexary/android_kernel_realme_sm8350 -b Agito kernel/oneplus/sm8350
+git clone https://github.com/IamCexary/kernel_oneplus_sm8350 -b fourteen-qpr1 kernel/oneplus/sm8350
 echo "Cloning Common Vendor Tree..."
-git clone --depth=1 https://github.com/IamCexary/vendor_oneplus_sm8350-common.git -b udc vendor/oneplus/sm8350-common
+git clone https://github.com/araujo791/vendor_oneplus_sm8350-common-SuperiorEx -b udc vendor/oneplus/sm8350-common
 echo "Cloning Vendor Tree..."
-git clone --depth=1 https://github.com/IamCexary/vendor_realme_porsche.git -b udc vendor/realme/porsche
+git clone https://github.com/araujo791/vendor_realme_porsche-SuperiorEx -b udc vendor/realme/porsche
 echo "Cloning OPlusExtras..."
-git clone https://github.com/IamCexary/packages_apps_OPlusExtras.git packages/apps/OPlusExtras
+git clone https://github.com/araujo791/packages_apps_OPlusExtras-SuperiorEx packages/apps/OPlusExtras
 echo "Cloning Firmware..."
-git clone --depth=1 https://gitlab.com/kajirokasuna/proprietary_vendor_realme_firmware.git -b uvite-porsche vendor/realme/firmware
+git clone https://gitlab.com/araujo791/proprietary_vendor_realme_firmware -b uvite-porsche vendor/realme/firmware
 echo "Cloning Oneplus Camera..."
-git clone --depth=1 https://gitlab.com/kajirokasuna/proprietary_vendor_oplus_camera.git -b topaz-porsche vendor/oplus/camera
+git clone https://gitlab.com/araujo791/proprietary-vendor-oplus-camera-superior-ex -b topaz-porsche vendor/oplus/camera
 echo "kernel-build-tools Fix error openssl/bio.h..."
 git clone https://github.com/PixelExperience-Staging/prebuilts_kernel-build-tools -b fourteen prebuilts/kernel-build-tools
 
